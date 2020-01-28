05f55d29653b20974142576e2f6de02c1df8dd20
Ruslan Rymar <ruslan.rimar@railsreactor.com>
2020-01-28 11:53:09 +0200

    akita added

--- AppTemplate.Web/ClientApp/src/app/app.module.ts
+++ AppTemplate.Web/ClientApp/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsersModule } from './users/users/users.module';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { UsersModule } from './users/users/users.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    UsersModule
+    UsersModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
