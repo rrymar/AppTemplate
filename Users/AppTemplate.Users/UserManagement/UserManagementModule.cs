981b1e820a53638c6d40230b5a60c71a1f0a3d8c
Ruslan <ruslan.rimar@gmail.com>
2020-07-27 13:46:46 +0300

    fix build

--- Users/AppTemplate.Users/UserManagement/UserManagementModule.cs
+++ Users/AppTemplate.Users/UserManagement/UserManagementModule.cs
@@ -6,7 +6,7 @@ namespace AppTemplate.Users.UserManagement
 {
     public class UserManagementModule : IModule
     {
-        public override void Register(IServiceCollection services)
+        public void Register(IServiceCollection services)
         {
             services.AddTransient<SearchUsersQueryHandler>();
             services.AddTransient<CreateUserHandler>();
