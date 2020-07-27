7950d9de1a7219487298f70af9ba7ee1261dc793
Ruslan <ruslan.rimar@gmail.com>
2020-07-27 12:00:52 +0300

    hide warning and remove search service

--- AppTemplate.Web/ClientApp/src/app/users/user-management/users-list/users-list.service.ts
+++ AppTemplate.Web/ClientApp/src/app/users/user-management/users-list/users-list.service.ts
@@ -1,14 +1,18 @@
-import { SearchService } from 'app/core/search/search.service';
 import { SearchQuery } from 'app/core/search/search-query';
 import { UserModel } from '../user.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ResultsList } from 'app/core/search/results-list';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class UsersListService extends SearchService<UserModel, SearchQuery>{
-  getBasePath = () => 'api/users';
+export class UsersListService {
 
-  constructor(httpClient: HttpClient) {
-    super(httpClient);
+  constructor(private httpClient: HttpClient) {
   }
+
+  search(query: SearchQuery): Observable<ResultsList<UserModel>> {
+    return this.httpClient.post<ResultsList<UserModel>>('api/Users/Search', query);
+  }
+
 }
